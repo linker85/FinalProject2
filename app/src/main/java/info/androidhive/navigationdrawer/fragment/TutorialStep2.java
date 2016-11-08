e52f380a8b6f7874cb925a72dc63a9d82afbe727
linker85 <linker85@hotmail.com>
2016-11-07 20:50:41 -0500

    finished new seeker bar

--- app/src/main/java/info/androidhive/navigationdrawer/fragment/TutorialStep2.java
+++ app/src/main/java/info/androidhive/navigationdrawer/fragment/TutorialStep2.java
@@ -14,13 +14,11 @@ import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.RelativeLayout;
-import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.etiennelawlor.discreteslider.library.ui.DiscreteSlider;
 import com.etiennelawlor.discreteslider.library.utilities.DisplayUtility;
-import com.nineoldandroids.animation.ValueAnimator;
 
 import org.codepond.wizardroid.WizardStep;
 import org.codepond.wizardroid.persistence.ContextVariable;
@@ -43,7 +41,6 @@ import rx.schedulers.Schedulers;
 public class TutorialStep2 extends WizardStep {
 
     private static final String TAG = "Step2TAG_";
-    private SeekBar  volumeControl;
     private TextView totalToPay;
     private TextView snap_bar;
     private RelativeLayout relativeLayout;
@@ -87,10 +84,10 @@ public class TutorialStep2 extends WizardStep {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.step_choose_time, container, false);
 
-        volumeControl                = (SeekBar)        v.findViewById(R.id.volume_bar);
         totalToPay                   = (TextView)       v.findViewById(R.id.total_to_pay);
         snap_bar                     = (TextView)       v.findViewById(R.id.snap_bar);
         relativeLayout               = (RelativeLayout) v.findViewById(R.id.parent_step);
+        saveTime                     = (Button)         v.findViewById(R.id.btn_save_time);
 
         discreteSlider1               = (DiscreteSlider) v.findViewById(R.id.discrete_slider1);
         tickMarkLabelsRelativeLayout1 = (RelativeLayout) v.findViewById(R.id.tick_mark_labels_rl);
@@ -116,11 +113,13 @@ public class TutorialStep2 extends WizardStep {
             finalMessage = "You´ve succesfully checkin.";
         }
 
-        if (isCheckin) {
-            setVolumeControlListener();
-        } else {
-            volumeControl.setVisibility(View.INVISIBLE);
+        if (!isCheckin) {
             totalToPay.setVisibility(View.INVISIBLE);
+            discreteSlider1.setVisibility(View.INVISIBLE);
+            discreteSlider2.setVisibility(View.INVISIBLE);
+            saveTime.setVisibility(View.INVISIBLE);
+            tickMarkLabelsRelativeLayout1.setVisibility(View.INVISIBLE);
+            tickMarkLabelsRelativeLayout2.setVisibility(View.INVISIBLE);
             snap_bar.setText("You need to Check in before extending time.");
         }
 
@@ -134,8 +133,6 @@ public class TutorialStep2 extends WizardStep {
         discreteSlider1.setPosition(0);
         discreteSlider2.setTickMarkCount(6);
         discreteSlider2.setPosition(0);
-
-        saveTime = (Button) view.findViewById(R.id.btn_save_time);
 
         saveTime.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -295,130 +292,6 @@ public class TutorialStep2 extends WizardStep {
                 addTickMarkTextLabels2();
             }
         });
-    }
-
-    private void setVolumeControlListener() {
-        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int progressChanged = 0;
-            double time         = 0;
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                progressChanged = progress;
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-
-            private ProgressDialog progressDialog;
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                double costPerMinutes = 1.5;
-                double total          = progressChanged * costPerMinutes;
-
-                if (progressChanged > 60) {
-                    time = (double) progressChanged / 60;
-                    if (time == 1) {
-                        totalToPay.setText("You have choosen: " + progressChanged + " - " + time + " hour, you have to pay: $" + total);
-                    } else {
-                        totalToPay.setText("You have choosen: " + progressChanged + " - " + time + " hours, you have to pay: $" + total);
-                    }
-                } else {
-                    time = progressChanged;
-                    if (time == 1) {
-                        totalToPay.setText("You have choosen: " + progressChanged + " - " + time + " minute, you have to pay: $" + total);
-                    } else {
-                        totalToPay.setText("You have choosen: " + progressChanged + " - " + time + " minutes, you have to pay: $" + total);
-                    }
-                }
-
-                progressDialog = new ProgressDialog(getActivity());
-
-                Snackbar snackbar = Snackbar
-                        .make(relativeLayout, "Do you accept?", Snackbar.LENGTH_LONG)
-                        .setAction("Yes", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                Observable<Success> resultSaveApiObservable = SaveApiRetroFitHelper.
-                                        Factory.createCheckInOut("581deb6b0f0000702a02daee"); // user
-                                resultSaveApiObservable
-                                        .subscribeOn(Schedulers.io())
-                                        .observeOn(AndroidSchedulers.mainThread())
-                                        .subscribe(new Subscriber<Success>() {
-                                            @Override
-                                            public void onStart() {
-                                                Log.d(TAG, "onStart: ");
-                                                progressDialog.setMessage("Loading...");
-                                                progressDialog.show();
-                                            }
-
-                                            @Override
-                                            public void onCompleted() {
-                                                Log.d(TAG, "onCompleted: ");
-                                            }
-
-                                            @Override
-                                            public void onError(Throwable e) {
-                                                Log.d(TAG, "onError: " + e.getMessage());
-                                                totalToPay.setText("An error ocurred, your time couldn´t be set.");
-                                            }
-
-                                            @Override
-                                            public void onNext(Success success) {
-                                                CheckinMock checkinMock = null;
-                                                ///////////////////////////////////// Simulation of registering time
-                                                SharedPreferences sharedPref = getActivity().
-                                                        getSharedPreferences("my_park_meter_pref", Context.MODE_PRIVATE);
-                                                String email = sharedPref.getString("email", "");
-                                                List<CheckinMock> checkinMockList = CheckinMock.findWithQuery(
-                                                        CheckinMock.class, "SELECT * FROM CHECKIN_MOCK WHERE EMAIL=?", email);
-                                                final SharedPreferences.Editor editor = sharedPref.edit();
-                                                editor.remove("checkin_temp");
-                                                editor.commit();
-                                                if (checkinMockList != null && !checkinMockList.isEmpty()) {
-                                                    checkinMock = checkinMockList.get(0);
-                                                } else {
-                                                    checkinMock = new CheckinMock();
-                                                    checkinMock.setResult(1);
-                                                    checkinMock.setEmail(email);
-                                                }
-                                                checkinMock.save();
-                                                ////////////////////////////////////////////////////////////////////
-                                                if (progressChanged > 60) {
-                                                    if (time == 1) {
-                                                        Toast.makeText(getActivity(), finalMessage, Toast.LENGTH_LONG).show();
-                                                    } else {
-                                                        Toast.makeText(getActivity(), finalMessage, Toast.LENGTH_LONG).show();
-                                                    }
-                                                } else {
-                                                    if (time == 1) {
-                                                        Toast.makeText(getActivity(), finalMessage, Toast.LENGTH_LONG).show();
-                                                    } else {
-                                                        Toast.makeText(getActivity(), finalMessage, Toast.LENGTH_LONG).show();
-                                                    }
-                                                }
-                                                getActivity().finish();
-                                                startActivity(getActivity().getIntent());
-                                            }
-                                        });
-                            }
-                        });
-                snackbar.show();
-            }
-        });
-    }
-
-    private static void setProgressAnimatedJdk(final SeekBar seekBar, int from, int to) {
-        ValueAnimator anim = ValueAnimator.ofInt(from, to);
-        anim.setDuration(100);
-        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                int animProgress = (Integer) animation.getAnimatedValue();
-                seekBar.setProgress(animProgress);
-            }
-        });
-        anim.start();
     }
 
     // region Helper Methods

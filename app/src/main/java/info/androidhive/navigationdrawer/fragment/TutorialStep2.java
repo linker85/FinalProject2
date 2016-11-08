ee5f1bab0fc3d92db6aaefccf38fe3a5ec16a662
linker85 <linker85@hotmail.com>
2016-11-07 20:35:10 -0500

    checkin seek bar improved

--- app/src/main/java/info/androidhive/navigationdrawer/fragment/TutorialStep2.java
+++ app/src/main/java/info/androidhive/navigationdrawer/fragment/TutorialStep2.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -46,9 +47,13 @@ public class TutorialStep2 extends WizardStep {
     private TextView totalToPay;
     private TextView snap_bar;
     private RelativeLayout relativeLayout;
+    private Button   saveTime;
 
     private String[] tickMarkLabels1 = {"0 min", "5 min", "10 min", "15 min", "30 min", "45 min"};
     private String[] tickMarkLabels2 = {"0 hr", "1 hr", "2 hr", "3 hr", "4 hr", "5 hr", "6 hr", "7 hr", "8 hr"};
+    private double[] costLabels1 = {0, 0.5, 1, 1, 1,5, 3, 4,5};
+    private double[] costLabels2 = {0, 10, 20, 30, 40, 50, 60, 70, 80};
+
     // region Views
     DiscreteSlider discreteSlider1;
     DiscreteSlider discreteSlider2;
@@ -129,19 +134,128 @@ public class TutorialStep2 extends WizardStep {
         discreteSlider1.setPosition(0);
         discreteSlider2.setTickMarkCount(6);
         discreteSlider2.setPosition(0);
+
+        saveTime = (Button) view.findViewById(R.id.btn_save_time);
+
+        saveTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectionHr  = 0;
+                int selectionMin = 0;
+
+                SharedPreferences sharedPref = getActivity().
+                        getSharedPreferences("my_park_meter_pref", Context.MODE_PRIVATE);
+                selectionMin = sharedPref.getInt("min", 0);
+                selectionHr  = sharedPref.getInt("hr", 0);
+
+                Log.d("TAG", "onPositionChangedMin: " + costLabels2[selectionHr]);
+                Log.d("TAG", "onPositionChangedHr: " + costLabels1[selectionMin]);
+
+                if (costLabels2[selectionHr] > 0 || costLabels1[selectionMin] > 0) {
+
+                    StringBuilder message = new StringBuilder("You have choosen ");
+                    double timeMinD = 0;
+                    double timeHrD = 0;
+
+                    if (costLabels2[selectionHr] > 0) {
+                        message.append(tickMarkLabels2[selectionHr]);
+                        timeHrD = costLabels2[selectionHr];
+                    }
+
+                    if (costLabels1[selectionMin] > 0) {
+                        if (costLabels2[selectionHr] > 0) {
+                            message.append(" and ");
+                        }
+                        message.append(tickMarkLabels1[selectionMin]);
+                        timeMinD = costLabels1[selectionMin];
+                    }
+                    message.append(", you have to pay: $" + (timeMinD + timeHrD));
+                    totalToPay.setText(message);
+
+                    final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+
+                    Snackbar snackbar = Snackbar
+                            .make(relativeLayout, "Do you accept?", Snackbar.LENGTH_LONG)
+                            .setAction("Yes", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Observable<Success> resultSaveApiObservable = SaveApiRetroFitHelper.
+                                            Factory.createCheckInOut("581deb6b0f0000702a02daee"); // user
+                                    resultSaveApiObservable
+                                            .subscribeOn(Schedulers.io())
+                                            .observeOn(AndroidSchedulers.mainThread())
+                                            .subscribe(new Subscriber<Success>() {
+                                                @Override
+                                                public void onStart() {
+                                                    Log.d(TAG, "onStart: ");
+                                                    progressDialog.setMessage("Loading...");
+                                                    progressDialog.show();
+                                                }
+
+                                                @Override
+                                                public void onCompleted() {
+                                                    Log.d(TAG, "onCompleted: ");
+                                                }
+
+                                                @Override
+                                                public void onError(Throwable e) {
+                                                    Log.d(TAG, "onError: " + e.getMessage());
+                                                    totalToPay.setText("An error ocurred, your time couldn´t be set.");
+                                                }
+
+                                                @Override
+                                                public void onNext(Success success) {
+                                                    CheckinMock checkinMock = null;
+                                                    ///////////////////////////////////// Simulation of registering time
+                                                    SharedPreferences sharedPref = getActivity().
+                                                            getSharedPreferences("my_park_meter_pref", Context.MODE_PRIVATE);
+                                                    String email = sharedPref.getString("email", "");
+                                                    List<CheckinMock> checkinMockList = CheckinMock.findWithQuery(
+                                                            CheckinMock.class, "SELECT * FROM CHECKIN_MOCK WHERE EMAIL=?", email);
+                                                    final SharedPreferences.Editor editor = sharedPref.edit();
+                                                    editor.remove("checkin_temp");
+                                                    editor.commit();
+                                                    if (checkinMockList != null && !checkinMockList.isEmpty()) {
+                                                        checkinMock = checkinMockList.get(0);
+                                                    } else {
+                                                        checkinMock = new CheckinMock();
+                                                        checkinMock.setResult(1);
+                                                        checkinMock.setEmail(email);
+                                                    }
+                                                    checkinMock.save();
+                                                    ////////////////////////////////////////////////////////////////////
+                                                    Toast.makeText(getActivity(), finalMessage, Toast.LENGTH_LONG).show();
+                                                    getActivity().finish();
+                                                    startActivity(getActivity().getIntent());
+                                                }
+                                            });
+                                }
+                            });
+                    snackbar.show();
+
+                } else {
+                    totalToPay.setText("Select a time before saving.");
+                }
+            }
+        });
+
         // Detect when slider position changes
         discreteSlider1.setOnDiscreteSliderChangeListener(new DiscreteSlider.OnDiscreteSliderChangeListener() {
             @Override
             public void onPositionChanged(int position) {
                 int childCount = tickMarkLabelsRelativeLayout1.getChildCount();
-                for (int i= 0; i<childCount; i++) {
+                for (int i = 0; i < childCount; i++) {
                     TextView tv = (TextView) tickMarkLabelsRelativeLayout1.getChildAt(i);
                     if (i == position)
                         tv.setTextColor(getResources().getColor(R.color.colorPrimary));
                     else
                         tv.setTextColor(getResources().getColor(R.color.grey_400));
                 }
-                Log.d("TAG", "onPositionChanged: " + tickMarkLabels1[position]);
+                SharedPreferences sharedPref = getActivity().
+                        getSharedPreferences("my_park_meter_pref", Context.MODE_PRIVATE);
+                final SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putInt("min", position);
+                editor.commit();
             }
         });
 
@@ -156,7 +270,11 @@ public class TutorialStep2 extends WizardStep {
                     else
                         tv.setTextColor(getResources().getColor(R.color.grey_400));
                 }
-                Log.d("TAG", "onPositionChanged: " + tickMarkLabels2[position]);
+                SharedPreferences sharedPref = getActivity().
+                        getSharedPreferences("my_park_meter_pref", Context.MODE_PRIVATE);
+                final SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putInt("hr", position);
+                editor.commit();
             }
         });
 
